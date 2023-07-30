de503ceebd959ff70dfd649c9ae3864fd5a8f7f1
limjunan <bryanlim080302@gmail.com>
2023-07-30 22:26:20 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3V_18wmpwXjlH7Ss0GwJialCJx317zrM",
+  authDomain: "netflix-clone-10885.firebaseapp.com",
+  projectId: "netflix-clone-10885",
+  storageBucket: "netflix-clone-10885.appspot.com",
+  messagingSenderId: "129150955379",
+  appId: "1:129150955379:web:1410ce92e320745e555f06"
+};
