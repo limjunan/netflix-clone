f71aa4f062e9c9f4387d5df51ea3924cac4d08cc
limjunan <bryanlim080302@gmail.com>
2023-08-19 15:19:28 +0800

    added homscreen and nav

---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./HomeScreen.css";
+
+function HomeScreen() {
+    return (
+        <div className="HomeScreen">
+            
+        </div>
+    )
+}
+
+export default HomeScreen
